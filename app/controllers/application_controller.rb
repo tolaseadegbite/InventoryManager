cd79cdc4ba9ef232c9e64d3b141b52fa840a5802
tolaseadegbite <tolasekelvin@gmail.com>
2025-01-14 15:42:04 +0100

    Install rodauth and style login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,36 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  # before_action :authenticate!
+
+  private
+
+  def set_layout
+    if rodauth.logged_in?
+      "application" # Layout for authentication-related pages
+    elsif authentication_page?
+      "authentication" # Layout for authentication-related pages
+    end
+  end
+
+
+
+  #######################################################################################################################
+  # rodauth
+  #######################################################################################################################
+
+  def authentication_page?
+    # Check if the current path matches Rodauth routes
+    request.path.start_with?("/auth") || rodauth.current_route.present?
+  end
+
+  def current_account
+    rodauth.rails_account
+  end
+  helper_method :current_account # skip if inheriting from ActionController::API
+
+  def authenticate!
+    rodauth.require_account # redirect to login page if not authenticated
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "static_pages#home"
+  
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
 end
