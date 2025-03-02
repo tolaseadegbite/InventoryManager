89d651f02448f7ab9f737f7790d8ef6b8d9cca03
tolaseadegbite <tolasekelvin@gmail.com>
2025-03-02 03:25:07 +0100

    Add links to dashbard stats card

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,10 @@ class ApplicationController < ActionController::Base
   
   include Authentication
   layout :set_layout
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
+
   helper_method :current_user
   before_action :set_accordion_inventories
   
