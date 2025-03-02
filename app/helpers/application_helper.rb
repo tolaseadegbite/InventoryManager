3ec664097b3eb4d1485aa5b9f6bf3053ef81f6b1
tolaseadegbite <tolasekelvin@gmail.com>
2025-03-02 12:50:49 +0100

    Create inventory history mobile view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,17 @@ module ApplicationHelper
       path: path
     }
   end
+
+  def action_type_badge_class(action_type)
+    case action_type.to_s
+    when 'add'
+      'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300'
+    when 'remove'
+      'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300'
+    when 'adjust'
+      'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300'
+    else
+      'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300'
+    end
+  end
 end
